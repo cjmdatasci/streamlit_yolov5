a3ed8c533523e96cfdf3fa6f15d9f8fb0c04e91b
cjmdatasci <cjm.datasci@gmail.com>
2021-11-06 01:44:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def get_yolo5(model_type='s'):
     torch model
         torch-модель типа `<class 'models.common.autoShape'>`
     '''
-    return torch.hub.load('ultralytics/yolov5', 'custom', path='bestcj5.pt')
+    return torch.hub.load('ultralytics/yolov5', 'custom', path='bestcj9.pt')
                           
 
 @st.cache(max_entries=10)
